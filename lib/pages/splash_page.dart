c8a73703f8c1db3d949d729366ebc9c485ab8673
Albani Rajata Malik <albeebro002@gmail.com>
2025-02-10 22:35:04 +0700

    loading app config, http service, movie service

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,5 +1,16 @@
+import 'dart:convert';
+
 //Packages
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:flutter/services.dart';
+
+//Services
+import '../services/http_service.dart';
+import '../services/movie_service.dart';
+
+//Model
+import '../models/app_config.dart';
 
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
@@ -24,7 +35,29 @@ class _SplashPageState extends State<SplashPage> {
       (_) => widget.onInitializationComplete(),
     );
   }
-  
+
+  Future<void> _setup(BuildContext _context) async {
+    final getIt = GetIt.instance;
+
+    final configFile = await rootBundle.loadString('assets/config/main.json');
+    final configData = jsonDecode(configFile);
+
+    getIt.registerSingleton<AppConfig>(
+      AppConfig(
+        BASE_API_URL: configData['BASE_API_URL'],
+        BASE_IMAGE_API_URL: configData['BASE_IMAGE_API_URL'],
+        API_KEY: configData['API_KEY'],
+      ),
+    );
+
+    getIt.registerSingleton<HTTPService>(
+      HTTPService(),
+    );
+
+    getIt.registerSingleton<MovieService>(
+      MovieService(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
